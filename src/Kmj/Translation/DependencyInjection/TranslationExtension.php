eb908fb7e7dc9790ecb26c2daa2b29f4baec8c86
kematjaya0 <kematjaya0@gmail.com>
2019-01-03 16:51:39 +0700

    initialize

---
+++ src/Kmj/Translation/DependencyInjection/TranslationExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of TranslationExtension
+ *
+ * @author NUR HIDAYAT
+ */
+
+namespace Kmj\Translation\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TranslationExtension extends Extension{
+    
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        
+    }
+}
---
+++ src/Kmj/Translation/TranslationBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Description of TranslationBundle
+ *
+ * @author NUR HIDAYAT
+ */
+
+namespace Kmj\Translation;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TranslationBundle extends Bundle {
+    
+    public function build(ContainerBuilder $container)
+    {
+        //$container->addCompilerPass(new SerializerConfigurationPass());
+    }
+    
+}
